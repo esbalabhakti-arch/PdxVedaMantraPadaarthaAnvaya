df88a57a92ebda92defa1d6a848e4a8e4da196ad
esbalabhakti-arch <esbalabhakti@gmail.com>
2026-02-21 14:49:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -121,6 +121,15 @@ const TOPIC_LIBRARY = [
         transcriptionDocx: "Images/112_7th_Panchadi_Part2_transcription.txt",
         summaryDocx: "Images/112_7th_Panchadi_Part2_summary.txt",
         note: "Aruna Prashnam - Panchadi 7 - Part2"
+      },
+      {
+        id: "113_Panchadi_8_Part1",
+        date: "2026-02-21",
+        title: "Aruna Prashnam - Panchadi 8 - Part1",
+        audio: "Audio/113_8th_Panchadi_Part1.mp4",
+        transcriptionDocx: "Images/113_8th_Panchadi_Part1_transcription.txt",
+        summaryDocx: "Images/113_8th_Panchadi_Part1_summary.txt",
+        note: "Aruna Prashnam - Panchadi 8 - Part1"
       }
     ]
   },
